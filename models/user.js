a12eaab2a0431c0b8d8eae63a20b81190e5cf4e5
Andrew Galyan-Mann <asgalyanmann@gmail.com>
2021-02-23 13:28:25 -0500

    add model validation

--- models/user.js
+++ models/user.js
@@ -6,19 +6,61 @@ module.exports = (sequelize) => {
   User.init({
     firstName: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A first name is required.'
+        },
+        notEmpty: {
+          msg: 'Please provide a first name.'
+        }
+      }
     },
     lastName: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A last name is required.'
+        },
+        notEmpty: {
+          msg: 'Please provide a last name.'
+        }
+      }
     },
     emailAddress: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      unique: {
+        msg: 'The emsil you entered already exist.'
+      },
+      validate: {
+        notNull: {
+          msg: 'An email is required.'
+        },
+        notEmpty: {
+          msg: 'Please provide an email.'
+        },
+        isEmail: {
+          msg: 'Please provide a valid email address.'
+        }
+      }
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A password is required.'
+        },
+        notEmpty: {
+          msg: 'Please provide a password.'
+        },
+        len: {
+          args: [8, 20],
+          msg: 'The password should be between 8 and 20 characters in lenght.'
+        }
+      }
     }
   }, { sequelize, timestamps: false });
 
